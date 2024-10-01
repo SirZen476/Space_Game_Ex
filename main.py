6427e530864a112e12bea74c7e949eaec5753fc3
SirZen476 <danielzenkin82@gmail.com>
2024-10-01 16:38:46 +0300

    removed old movment systems, cleanup

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from Tools.scripts.dutree import display
 from sympy.core.random import randint
 
 #general setup
-#pygame.init()# init pygame
-window_width,window_height = 1280, 720#screen size
+#pygame.init()# init pygame, causes freeze at start - neet to see if something goes wrong
+window_width,window_height = 1280, 720 #screen size
 screen = pygame.display.set_mode((window_width,window_height))# create screen
 pygame.display.set_caption('Space Shooter')
 running = True
@@ -18,13 +18,15 @@ surf.fill('orange')
 # import images
 player_surf = pygame.image.load('source_files/images/player.png')
 player_rect = player_surf.get_rect(center = (window_width/2,window_height-100))# rect to control position
+# for player movment - may change
 player_speed = 250
-player_dir = []
+player_dir = []# ends up as a list of 4 vectors for movment in diffrent axis
 player_dir.append(pygame.math.Vector2(0,1))#down index 0
 player_dir.append(pygame.math.Vector2(2,0))# right 1
 player_dir.append(pygame.math.Vector2(0,-1))# up 2
 player_dir.append(pygame.math.Vector2(-2,0))# left 3
 
+#load objects
 
 laser_surf = pygame.image.load('source_files/images/laser.png')
 laser_rect = laser_surf.get_rect(center = (window_width/2,window_height-200))
@@ -36,51 +38,34 @@ star_surf = pygame.image.load('source_files/images/star.png')
 star_pos= [(randint(0,window_width),randint(0,window_height)) for i in range(20)]
 
 
-
 while running:
     dt = clock.tick(FPS_target) /1000
     #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        '''
-        if event.type == pygame.KEYDOWN:# key press movment
-            if event.key == pygame.K_DOWN and player_rect.bottom < window_height:# lefy
-                player_rect.center += player_dir[0] * player_speed * dt
-            if event.key == pygame.K_RIGHT and player_rect.right < window_width:# right
-                player_rect.center += player_dir[1] * player_speed * dt
-            if event.key == pygame.K_UP and player_rect.top > 0:# up
-                player_rect.center += player_dir[2] * player_speed * dt
-            if event.key == pygame.K_LEFT and player_rect.left > 0:# left
-                player_rect.center += player_dir[3] * player_speed * dt
-        if event.type == pygame.MOUSEMOTION:
-            player_rect.center = event.pos
-''' # diffrent implementation:
 
-    #player_rect.center = pygame.mouse.get_pos() # mouse movment
+
     keys = pygame.key.get_pressed()
+
     if keys[pygame.K_DOWN] and player_rect.bottom < window_height :
         player_rect.center += player_dir[0] * player_speed * dt
+        player_dir2 = pygame.math.Vector2(0,1)
     if keys[pygame.K_RIGHT] and player_rect.right < window_width:# right
         player_rect.center += player_dir[1] * player_speed * dt
+        player_dir2 = pygame.math.Vector2(1,0)
     if keys[pygame.K_UP]  and player_rect.top > 0:# up
         player_rect.center += player_dir[2] * player_speed * dt
+        player_dir2 = pygame.math.Vector2(0,-1)
     if keys[pygame.K_LEFT] and player_rect.left > 0:# left
         player_rect.center += player_dir[3] * player_speed * dt
-
 
     #draw game
     screen.fill('azure3')#fill with blue color
     for pos in star_pos :
         screen.blit(star_surf,pos)
-    #random x and y movement
-    '''
-    player_rect.center += player_dir * player_speed * dt# player movment with vector
-    if player_rect.right > window_width or player_rect.left < 0:
-        player_dir[0] *= -1
-    if player_rect.top < 0 or player_rect.bottom > window_height:
-        player_dir[1] *= -1
-    '''
+
+
     screen.blit(player_surf,player_rect)
     screen.blit(laser_surf,laser_rect)
     screen.blit(meteor_surf,meteor_rect)
