9db63ddb737f9590c741c5860daaaf4724b82950
SirZen476 <danielzenkin82@gmail.com>
2024-10-01 12:57:33 +0300

    Created window, need a minute to load with pygam.init()

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #project was made with files from Clear code
 #https://github.com/clear-code-projects/5games
 
+import pygame
+
+#general setup
+pygame.init()# init pygame
+window_width,window_height = 1280, 720#screen size
+screen = pygame.display.set_mode((window_width,window_height))# create screen
+
+while True:
+    pass
