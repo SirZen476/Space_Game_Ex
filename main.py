1e6fd5fcd9d1f32a3939e5bec53bec13fb26a9cc
SirZen476 <danielzenkin82@gmail.com>
2024-10-01 17:57:08 +0300

    cleanup and small changes( changed and Meteor Star class so it recives an image to not load the same thing every time)

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ class Player(Sprite):
         self.rect.center += self.direction * self.speed * dt
 
 class Star(Sprite):
-    def __init__(self,groups,position):
+    def __init__(self,groups,image):
         super().__init__(groups)
-        self.image = pygame.image.load('source_files/images/star.png')
-        self.rect = self.image.get_rect(center = position)
+        self.image = image
+        self.rect = self.image.get_rect(center = (randint(0,window_width),randint(0,window_height)))
 
 
 class Laser(Sprite):
@@ -49,9 +49,9 @@ class Laser(Sprite):
         self.rect.center += self.direction * self.speed * dt
 
 class Meteor(Sprite):
-    def __init__(self,groups,x,y):
+    def __init__(self,groups,x,y,image ):
         super().__init__(groups)
-        self.image = pygame.image.load('source_files/images/meteor.png')
+        self.image = image
         self.rect = self.image.get_rect(center = (x,y))
         self.speed = 300
         self.direction = pygame.math.Vector2(0,1)
@@ -71,15 +71,17 @@ pygame.display.set_caption('Space Shooter')
 running = True
 clock =pygame.time.Clock()  # control framerate, control
 FPS_target = 99
-# create  player and items
+# create  Group
 all_sprites = pygame.sprite.Group()
+# add items to game
+laser = Laser(all_sprites,20,window_height-20)
+image_star = pygame.image.load('source_files/images/star.png')# load before so we wont have to load 20 times the same image
+for i in range(20):
+    star = Star(all_sprites,image_star)
+image_meteor = pygame.image.load('source_files/images/meteor.png')
+meteor = Meteor(all_sprites,window_width/2,window_height/2, image_meteor)
 player = Player(all_sprites)
 
-laser = Laser(all_sprites,20,window_height-20)
-
-meteor = Meteor(all_sprites,window_width/2,window_height/2)
-for i in range(20):
-    star = Star(all_sprites,(randint(0,window_width),randint(0,window_height)))
 
 
 # game loop
@@ -89,7 +91,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:  # single time use- so key press will register just once # fire lasr - need to implement
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:  # fire laser - just one press
             laser_fire = Laser(all_sprites,player.rect.centerx,player.rect.centery)
             laser_fire.fire()
 
